4c440600a06045ac3a98e658d69dd7dc96978576
Daniel Rodriguez <daniel@danielfrg.com>
2022-01-11 20:12:14 -0600

    Add counter end

--- components/Counter.js
+++ components/Counter.js
@@ -25,5 +25,10 @@ export default function Counter() {
   const minutes = String(parseInt(diff.minutes)).padStart(2, "0");
   const seconds = String(parseInt(diff.seconds)).padStart(2, "0");
 
+  if (hours == 0 && minutes == 0 && seconds == 0) {
+    // Juego nuevo listo
+    return <span>Nuevo Wordle disponible!</span>;
+  }
+
   return <span>{`${hours}:${minutes}:${seconds}`}</span>;
 }
